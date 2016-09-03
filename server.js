1e61da888671c10884527d6662cbaa6c57d39077
Vasisht A. Shastry <shastryucsb@gmail.com>
2016-09-03 17:46:53 -0500

    fixed localhost

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ var app = express();
 
 var port = process.env.PORT || 3000
 
-app.use(function (req, res, next){
-    if(req.headers['x-forwarded-proto'] === 'http'){
-        next();
-    } else {
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
     }
 });
 
 app.use(express.static('public'));
 
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('listening on port' + ' ' + port);
 });
