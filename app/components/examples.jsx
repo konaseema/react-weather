532afe546fcc10f6d010bbd1aca102843ecbf7c8
Vasisht A. Shastry <shastryucsb@gmail.com>
2016-09-05 20:14:11 -0500

    added styling to examples

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router')
 
 // var Examples = React.createClass({
 //     render: function () {
@@ -12,8 +13,16 @@ var React = require('react');
 var Examples = (props) => {
     return (
         <div>
-        <h3>Examples</h3>
-        <p>Welcome to examples!</p> 
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out: </p>
+            <ol>
+                <li>
+                    <Link to='/?location=Naperville'>Naperville, IL</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Aurora'>Aurora, IL</Link>
+                </li>
+            </ol>
         </div>
     )
 };
