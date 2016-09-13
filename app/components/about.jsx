b75abe18c6d5348cd1c8f4c9706471a4dd3e268c
Vasisht A. Shastry <shastryucsb@gmail.com>
2016-09-12 20:59:47 -0500

    add styles for pages

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-        <h1 className="text=center">Welcome to the About Page!</h1>
+        <h1 className="text=center page-title">Welcome to the About Page!</h1>
         <p>Here is the filler text for the application I have created.
         Here we display the weather based on an input supplied by the user</p>
         </div>
